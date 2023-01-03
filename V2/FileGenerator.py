85d9a049a2e594a20efc6fef7b2c8fd71484efec
Ananya Kharche <106643078+narneon@users.noreply.github.com>
2023-01-03 00:00:10 -0800

    Makes Relevant Files

--- V2/FileGenerator.py
+++ V2/FileGenerator.py
@@ -2,7 +2,7 @@ import json
 import os
 import tempfile
 from pydub import AudioSegment
-
+import shutil
 
 # Difficulty names and ranks recognized by Beat Saber
 POSSIBLE_DIFF_NAMES = ['Easy', 'Normal', 'Hard', 'Expert', 'ExpertPlus']
@@ -15,28 +15,80 @@ DIFFICULTY_RANKS = {
 }
 
 
-# Creates the folder that info.dat, difficulty, and song.ogg are placed into, if it does not already exist
+# Main method that calls the others as necessary
+# difficulties should be sorted from Easy to ExpertPlus
+
+def mkFiles(songfile, songInfo, mapInfo, difficulties):
+    createMapFolder()
+    createInfoDat(songInfo, mapInfo, difficulties)
+    createDifficulties(difficulties)
+    exportAudioFile(songfile)
+
+
+# Creates the folder that info.dat, difficulty, and song.ogg are placed into
 def createMapFolder():
-    pass
+    if os.path.exists('export'):
+        shutil.rmtree('export')
+    os.mkdir('export')
 
 
 # Creates the appropriate info.dat
-def createInfoDat():
-    pass
+def createInfoDat(songInfo, mapInfo, difficulties):
+    f = open('export\\Info.dat', 'w')
+
+    # Creating barebones JSON version of Info.dat
+    infoJSON = {
+        '_version': '2.0.0',
+        '_songName': songInfo['name'],
+        '_songSubName': songInfo['subName'],
+        '_songAuthorName': songInfo['artist'],
+        '_levelAuthorName': 'AutoBeat',
+        '_beatsPerMinute': songInfo['bpm'],
+        '_shuffle': 0,
+        '_shufflePeriod': 0.5,
+        '_previewStartTime': mapInfo['pStart'],
+        '_previewDuration': mapInfo['pDur'],
+        '_songFilename': songInfo['audio'],
+        '_coverImageFilename': songInfo['cover'],
+        '_environmentName': mapInfo['env'],
+        '_allDirectionsEnvironmentName': 'GlassDesertEnvironment',
+        '_songTimeOffset': 0,
+        '_difficultyBeatmapSets': [
+            {
+                "_beatmapCharacteristicName": "Standard",
+                "_difficultyBeatmaps": []
+            }
+        ]
+    }
+
+    for diff in difficulties:
+        difficultyJSON = {
+            "_difficulty": diff['diff'],
+            "_difficultyRank": DIFFICULTY_RANKS[diff['diff']],
+            "_beatmapFilename": diff['diff'] + diff['type'] + '.dat',
+            "_noteJumpMovementSpeed": diff['njs'],
+            "_noteJumpStartBeatOffset": diff['offset']
+        }
+        infoJSON['_difficultyBeatmapSets'][0]['_difficultyBeatmaps'].append(difficultyJSON)
+    f.write(json.dumps(infoJSON, indent=2))
+    f.close()
 
 
 # Create empty difficulty.dat files
-def createDifficulties():
-    pass
+def createDifficulties(difficulties):
+    for diff in difficulties:
+        open('export\\' + diff['diff'] + diff['type'] + '.dat', 'w')
 
 
-def exportAudioFile(filename: str):
+def exportAudioFile(songfile: str):
     """
     Exports the audio file as song.ogg, and places it appropriately
     """
-    with open(filename, "r") as audiofile:
+    with open(songfile, "r") as audiofile:
         data = audiofile.read()
     f = tempfile.NamedTemporaryFile(delete=False)
     f.write(data)
-    AudioSegment.from_mp3(f.name).export('export/result.ogg', format='ogg')
+    AudioSegment.from_mp3(f.name).export('export\\song.ogg', format='ogg')
     f.close()
+
+
---
+++ V2/Main.py
@@ -0,0 +1,37 @@
+from FileGenerator import mkFiles
+
+# To be user inputted
+songInfo = {
+    'name': 'SONGNAME',
+    'subName': '',
+    'artist': 'ARTIST',
+    'bpm': 128,
+    'audio': 'song.ogg',
+    'cover': ''
+}
+
+mapInfo = {
+    'pStart': 1,
+    'pDur': 10,
+    'env': 'DefaultEnvironment',
+}
+
+eplusInfo = {
+        'type': 'Standard',
+        'diff': 'ExpertPlus',
+        'njs': 20,
+        'offset': .067
+}
+
+easyInfo = {
+        'type': 'Standard',
+        'diff': 'Easy',
+        'njs': 16,
+        'offset': .067
+}
+
+# difficulties should be sorted from Easy to ExpertPlus
+difficulties = [easyInfo, eplusInfo]
+
+mkFiles('look.ogg', songInfo, mapInfo, difficulties)
+
